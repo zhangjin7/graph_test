49f503c9d174d1f5a4fa025829abe36733c60e0d
zhangjin <347903476@qq.com>
2019-07-23 15:17:50 +0800

    first

---
+++ datasource.py
@@ -0,0 +1,33 @@
+import psycopg2
+import configparser
+
+class GreenPlum_Datasource:
+    def __init__(self):
+        self.conf=configparser.ConfigParser()
+        self.conf.read("properties.conf",encoding="utf-8")
+        self.host=self.conf.get("datasource","host")
+        self.port=self.conf.get("datasource","port")
+        self.user=self.conf.get("datasource","user")
+        self.passwd=self.conf.get("datasource","passwd")
+        self.database=self.conf.get("datasource","database")
+        self.conn=psycopg2.connect(host=self.host,port=self.port,database=self.database,user=self.user,password=self.passwd)
+        self.cursor=self.conn.cursor()
+        print(self.host+":"+self.port+"\\"+self.database)
+
+    def get_data(self,data_type):
+        sql=self.conf.get("sql",data_type)
+        self.cursor.execute(sql)
+        result=self.cursor.fetchall()
+        return result
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+if __name__=="__main__":
+    ds=GreenPlum_Datasource()
+    result=ds.get_data("daughter_rel")
+    print(result)
+    ds.close()
+
+
